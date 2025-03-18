21643e32dc4fc0b279334f757e24a93a7a5bb100
user <kimjh1020@g.shingu.ac.kr>
2025-03-18 12:10:36 +0900

    리스트

--- 02type.py
+++ 02type.py
@@ -5,7 +5,7 @@
   #          문자열 : str (string)
   #          논리값: bool (boolean)
 
-title = "시간계산"
+title = "시간 계산"
 sec = 3700
 min = sec/60
 bigger = min > sec
@@ -31,6 +31,7 @@ print(f'{seca=},{secs=},{secm=},{secd=},{secq=},{secr=}') #f string
 # tc = (tf - 32) * 5/9
 # tf = (tc * 9/5) +32
 tf = 100
+
 tc = (tf -32) * 5/9
 print(f'{tf=} ===> {tc=}')
 tc = 37.77777777777778
@@ -39,5 +40,15 @@ tf = (tc * 9/5) +32
 print(f'{tc=} ===> {tf=}')
 
 print(2 ** 3,2 ** (1/2))
-# 사칙연산중에서 0으로 나누는것은 허용하지 않음
-print(4/0)
+# 사칙 연산 중에서 0으로 나누는 것은 허용 하지 않음
+# print(3/0)
+a = True
+print(type(a))
+
+a='3'
+b=float(a)
+print(b**int(a))
+
+a = '3.5'
+b = 4
+print(a * b)
