819999d5dba347b9026b8be8e48cfebf94eb8aff
user <kimjh1020@g.shingu.ac.kr>
2025-03-19 14:59:15 +0900

    변수,타입,출력

--- 03list.py
+++ 03list.py
@@ -39,14 +39,27 @@ c1,c2,c3, _,_,_, = colors
 print(c1,c2,c3)
 
 # 다차원 리스트
+colors = ['red','green','blue']
+cities = ['서울','부산','인천','의정부','대전','강릉','논산','포항']
+combi = [ colors , cities ]
 
-#퀴즈
-first = ["egg", "salad", "bread", "soup", "canafe"]
+print(combi)
+print(combi[1][2]) # 인천
+# print(combi[2][3]) # 에러발생
+bigcombi = [combi, [0,2,7]]
+print(bigcombi)
+print(len(bigcombi))
+print(bigcombi[0][1][2]) # 인천
+print(bigcombi[1][1]) # 2
+# 퀴즈
+first = ["egg", "salad", "bread", "soup", "coffee"]
 second = ["fish", "lamb", "pork", "beef", "chicken"]
 third = ["apple", "banana", "orange", "grape", "mango"]
 
 order = [first, second, third]
-john = [order[0][:-2], second[1::3], third[0]]
-del john[2]
-john.extend([order[2][0:1]])
+john = [order[0][-2], second[1::3], third[0]] # john = [soup, [lamb,chicken], apple]
 print(john)
+del john[2] # john = [soup, [lamb,chicken]]
+john.extend([order[2][0:1]]) # john = [soup, [lamb,chicken], [apple]]
+print(john)
+
