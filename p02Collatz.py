d867a970068c93772d7879e9373663c24caed12f
user <kimjh1020@g.shingu.ac.kr>
2025-04-08 12:25:03 +0900

    우박수 프로젝트 시작

--- p02Collatz.py
+++ p02Collatz.py
@@ -48,6 +48,42 @@ values = backup
 index = values.index(115)
 print('2번째로 많은 단계수의 숫자',index+1)
 
+maxvalue = 0
+maxvaluen = 0
+maxvalue2=0
+maxvaluen2 = 0
+
+
+n = 0
+if n % 2 == 0:
+    print('짝수')
+else:
+    print("홀수")
+l = 0
+for n in range(1,100):
+    l=0
+    nsave =n
+    while n != 1 :
+        if n % 2 == 0:
+            n = n / 2
+            l = l + 1
+        elif n % 2 == 1:
+            n = 3 * n + 1
+            l = l + 1
+        else:
+            break
+    #가장 많은 단계를 가진 수 구하기
+    if maxvalue < l:
+        maxvalue2 = maxvalue
+        maxvalue = l
+        maxvaluen2 = maxvaluen
+        maxvaluen = nsave
+
+    elif maxvalue2 < l:
+        maxvalue2 = l
+
+print(f'{maxvalue=},{maxvaluen=}')
+print(f'{maxvalue2=},{maxvaluen2=}')
 
 
 
