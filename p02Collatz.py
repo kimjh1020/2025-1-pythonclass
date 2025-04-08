c9f4cb9837937f5e1cc1ac84c79a1db9f556520d
user <kimjh1020@g.shingu.ac.kr>
2025-04-08 11:54:26 +0900

    우박수 프로젝트 시작

--- p02Collatz.py
+++ p02Collatz.py
@@ -6,28 +6,43 @@
 #  예: 5 -> 16 ->8 ->4 ->2 ->1 (5단계)
 
 
-
-n = int(input("숫자를 입력하세요: "))
+maxvalue = 0
+maxvaluen = 0
+values = []
+n = 0
 if n % 2 == 0:
     print('짝수')
 else:
     print("홀수")
 l = 0
+for n in range(1,100):
+    print('n=',f'{n}')
+    l=0
+    nsave =n
+    while n != 1 :
+        if n % 2 == 0:
+            n = n / 2
+            l = l + 1
+        elif n % 2 == 1:
+            n = 3 * n + 1
+            l = l + 1
+        else:
+            break
+    values.append(l)
+    print(l, '단계')
+    #가장 많은 단계를 가진 수 구하기
+    if maxvalue < l:
+        maxvalue = l
+        maxvaluen = nsave
 
 
-while n != 1 :
-    if n % 2 == 0:
-        n = n / 2
-        print(n)
-        l = l + 1
-    elif n % 2 == 1:
-        n = 3 * n + 1
-        print(n)
-        l = l + 1
-    else:
-        break
 
-print(l,'단계')
+
+print(f'{maxvalue=},{maxvaluen=}')
+print(values)
+values.sort(reverse=True)
+print('2ndvalue=',values[1],'2ndvaluen')
+
 
 
 
